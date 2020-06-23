ad30af4a647883efb0c1263d11b2e85035f28853
ThugDenGamer <58256550+ThugDenGamer@users.noreply.github.com>
2020-06-23 19:44:14 +0400

    Create ekok.js

---
+++ ekok.js
@@ -0,0 +1,45 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let a;
+let b;
+
+readline.question('1. sayı: ', (sayi) => {
+    if(isNaN(sayi)){
+        console.log('bu bir sayı değil, lütfen tekrar dene')
+        readline.resume()
+    } else {
+        a = sayi
+        readline.question('2. sayı: ', (sayi) => {
+            if (isNaN(sayi)) {
+                console.log('bu bir sayı değil, lütfen tekrar dene')
+                readline.resume()
+            } else {
+                b = sayi
+                ekok(a,b)
+                readline.close()
+            }
+        })   
+    }
+})
+
+function ekok(a,b){
+    if(isNaN(a) || isNaN(b)) return console.log('a veya b sayı değil')
+    let ks;
+    let bs;
+    if(a > b) {
+        bs = a;
+        ks = b;
+    } else {
+        bs = b;
+        ks = a;
+    }
+    for(let i = 1; i<a*b+1; i++){
+        if(bs * i % ks == 0){
+            return console.log(`${a} ve ${b} nin ekoku: ${bs*i}`)
+        }
+    }
+}
+// ThugDenGamer tarafından yapıldı! ©️
